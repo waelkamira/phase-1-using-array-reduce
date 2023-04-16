f871bf81336d0e796e8471455f30a83d8904662f
wael kamira <ramondm4@gmail.com>
2023-04-16 23:38:17 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const batteries = [
+    { type: 'AA', amount: 12 },
+    { type: 'AAA', amount: 7 },
+    { type: 'AA', amount: 6 },
+    { type: 'D', amount: 3 },
+    { type: 'AAA', amount: 3 }
+  ];
+  
+  function reducer(batteryArray) {
+    const total = batteryArray.reduce(function(acc, battery) {
+      return acc + battery.amount;
+    }, 0);
+  
+    return total;
+  }
+  
+  const totalBatteries = reducer(batteries);
+  console.log(totalBatteries); // Output: 31
+  
